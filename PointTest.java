2ef53215b63e13c49347366c2e30e18ac0e445de
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-04-04 22:10:17 +0700

    pf-java-oop point2d point 3d

--- PointTest.java
+++ PointTest.java
@@ -1,5 +1,15 @@
 public class PointTest {
     public static void main(String[] args) {
+        Point2D point2D = new Point2D(0.0f, 0.0f);
+        System.out.println(point2D.toString());
 
+        Point2D point2D1 = new Point2D(5.0f, 6.0f);
+        System.out.println(point2D1.toString());
+
+        Point3D point3D = new Point3D(3.0f);
+        System.out.println(point3D);
+
+        Point3D point3D1 = new Point3D(4.0f, 5.0f, 3.0f);
+        System.out.println(point3D1);
     }
 }
